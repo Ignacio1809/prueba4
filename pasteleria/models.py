a23ef8f14747a499a904f2728aeda3b22d874d03
usuario <hola@gmail.com>
2021-07-02 00:05:22 -0400

    Se actualiza el models por emilio romero

--- pasteleria/models.py
+++ pasteleria/models.py
@@ -6,7 +6,7 @@ from django.db.models.base import Model
 # Create your models here.
 
 class Cliente(models.Model):
-    rut= models.CharField(max_length=100, default="")
+    rut = models.CharField(max_length=100, default="")
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     fecha_nacimiento = models.DateTimeField("Fecha de nacimiento")
@@ -16,4 +16,12 @@ class Cliente(models.Model):
 
     def __str__(self):  
         return self.rut
+
+class Producto(models.Model):
+    nombre_producto = models.CharField(max_length=100, default="")
+    precio = models.IntegerField(default=0)
+    descripcion = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.nombre_producto
 
