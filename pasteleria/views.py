4c32010f5dba2a9fb2f0ec6b521ef3d50b88b7b0
joshua ramirez <josh.ramirez@duocuc.cl>
2021-07-02 00:55:13 -0400

    Se actualiza views.py

--- pasteleria/views.py
+++ pasteleria/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from pasteleria.models import Cliente
+from pasteleria.models import Cliente, Producto
 from django.utils import timezone
 from django.urls import reverse
 
@@ -17,6 +17,11 @@ def agregar_clientes(request):
     carrito = {"clientes": clientes}
     return render(request, "pasteleria/agregar.html", carrito)
 
+def agregar_producto(request):
+    productos = Producto.objects.all()
+    carrito = {"producto": productos}
+    return render(request, "pasteleria/agregar_producto.html", carrito)
+
 def registrar_clientes(request):
     rut = request.POST["rut"]
     nombre = request.POST["nombre"]
@@ -29,22 +34,75 @@ def registrar_clientes(request):
     agregar.save()
     return HttpResponseRedirect(reverse('pasteleria:inicio'))
 
+def registrar_producto(request):
+    nombre_producto = request.POST["nombre_producto"]
+    precio = request.POST["precio"]
+    descripcion= request.POST["descripcion"]
+    agregar = Producto(nombre_producto=nombre_producto, precio=precio, descripcion= descripcion)
+    agregar.save()
+    return HttpResponseRedirect(reverse('pasteleria:inicio'))
+
 def buscar_clientes(request):
     return render(request, "pasteleria/buscar.html")
+    
+def buscar_producto(request):
+    return render(request, "pasteleria/buscar_producto.html")
+
 
 def mostrar_clientes(request):
     mostrar = request.POST["rut"]
     listado = Cliente.objects.filter(rut__startswith=mostrar)
     lista = {"listado": listado}
     return render(request, "pasteleria/mostrar.html", lista)
+
+def mostrar_producto(request):
+    mostrar = request.POST["nombre_producto"]
+    listado = Producto.objects.filter(nombre_producto__startswith=mostrar)
+    lista = {"listado": listado}
+    return render(request, "pasteleria/mostrar_producto.html", lista)
     
 def eliminar_clientes(request):
     return render(request, "pasteleria/eliminar.html")
 
+def eliminar_producto(request):
+    return render(request, "pasteleria/eliminar_producto.html")
+
 def eliminar(request):
-    borrar = Cliente.objects.get(rut=request.POST["eliminar"])
+    borrar = Cliente.objects.all(rut=request.POST["eliminar"])
     borrar.delete()
     return HttpResponse("Cliente eliminado")
+
+def actualizar(request):
+    actualizar = Cliente.objects.all()
+    mostrar = {"actualizar": actualizar}
+    return render(request, "pasteleria/actualizar.html", mostrar)
+
+def editar(request):
+    mostrar = Cliente.objects.get(rut= request.POST['rut'])
+    nombre = request.POST['nombre']
+    nombre = nombre.replace(' ','')
+    apellido = request.POST['apellido'] 
+    apellido = apellido.replace(' ','')
+    fecha = request.POST['fecha']
+    email= request.POST['correo']
+    email = email.replace(' ','')
+    dirrecion = request.POST['dirrecion']
+    comuna = request.POST['comuna'] 
+
+    mostrar.nombre = nombre
+    mostrar.apellido = apellido
+    mostrar.fecha = fecha
+    mostrar.email = email
+    mostrar.direccion = dirrecion
+    mostrar.comuna = comuna
+
+    mostrar.save()
+    return render(request, 'pasteleria/editar.html',)
+
+def borrar_pro(request):
+    borrar_producto = Producto.objects.get(nombre_producto=request.POST["borrar_pro"])
+    borrar_producto.delete()
+    return HttpResponse("Producto eliminado")
 
 
 
