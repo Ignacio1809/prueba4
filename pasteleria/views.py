6c5e36fb0aba1639586783a07171ed35b2b0add9
Ignacio <Ignacio@prueba.com>
2021-07-04 23:24:26 -0400

    Se actualizan las vistas del proyecto

--- pasteleria/views.py
+++ pasteleria/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from pasteleria.models import Cliente, Producto
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def index(request):
-    listado_clientes = Cliente.objects.all()
-    context = {"listado_clientes": listado_clientes}
-    return render(request, "pasteleria/inicio.html", context)
+    if request.user.has_perm('pasteleria.Admin') or ('pasteleria.view_cliente'):
+        listado_clientes = Cliente.objects.all()
+        context = {"listado_clientes": listado_clientes}
+        return render(request, "pasteleria/inicio.html", context)
+    else:
+        return ('No cuentas con los permisos')
 
 def agregar_clientes(request):
     clientes = Cliente.objects.all()
@@ -104,7 +107,58 @@ def borrar_pro(request):
     borrar_producto.delete()
     return HttpResponse("Producto eliminado")
 
+def actualizar_producto(request):
+    actualizar = Producto.objects.all()
+    mostrar = {"actualizar": actualizar}
+    return render(request, "pasteleria/actualizar_producto.html", mostrar)
+
+def editar_producto(request):
+    mostrar = Producto.objects.get(nombre_producto= request.POST['nombre_producto'])
+    nombre_producto = request.POST['nombre_producto']
+    nombre_producto = nombre_producto.replace(' ','')
+    precio = request.POST['precio'] 
+    precio = precio.replace(' ','')
+    descripcion = request.POST['descripcion'] 
+    descripcion = descripcion.replace(' ','')
+
+    mostrar.nombre_producto = nombre_producto
+    mostrar.precio = precio
+    mostrar.descripcion = descripcion
+
+    mostrar.save()
+    carrito = {"mostrar": mostrar}
+    return render (request, "pasteleria/editar_producto.html", carrito)
+
+def frm_iniciar_sesion(request):
+    return render(request, 'pasteleria/frm_iniciar_sesion.html')
+
+def iniciar_sesion(request):
+    usuario = request.POST["usuario"]
+    clave = request.POST["clave"]
+
+    user = authenticate(request, username = usuario, password = clave)
+
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse('pasteleria:inicio'))
+    else:
+        return HttpResponse ("No se pudo autenticar las credenciales")
+
+def producto_uno(request):
+    return render(request, "pasteleria/producto_uno.html")
+
+def producto_dos(request):
+    return render(request, "pasteleria/producto_dos.html")
 
 
-    
+def producto_tres(request):
+    return render(request, "pasteleria/producto_tres.html")
 
+def finalizar_pedido_uno(request):
+    return render(request, "pasteleria/finalizar_pedido_uno.html")
+
+def finalizar_pedido_dos(request):
+    return render(request, "pasteleria/finalizar_pedido_dos.html")
+
+def finalizar_pedido_tres(request):
+    return render(request, "pasteleria/finalizar_pedido_tres.html")
