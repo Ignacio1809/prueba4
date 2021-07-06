ec6dda94430500aa6785c282c5e645337c111158
Ignacio <Ignacio@prueba.com>
2021-07-05 21:35:29 -0400

    Se actualizan los modelos del proyecto

--- pasteleria/models.py
+++ pasteleria/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.db.models.base import Model
+from django.utils.translation import ugettext as _
 
 
 # Create your models here.
@@ -17,6 +18,16 @@ class Cliente(models.Model):
     def __str__(self):  
         return self.rut
 
+    class Meta:
+        permissions = (
+            ('Admin', _('Es el Admin')),
+            ('Usuario', _('Eres un simple usuario')),
+            ('Agregar', _('Puedes agregar clientes')),
+            ('Eliminar', _('Puedes eliminar clientes')),
+            ('Actualizar', _('Puedes actualizar clientes')),
+            ('Buscar', _('Puedes buscar productos')),
+        )
+
 class Producto(models.Model):
     nombre_producto = models.CharField(max_length=100, default="")
     precio = models.IntegerField(default=0)
@@ -24,4 +35,14 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre_producto
+
+    class Meta:
+        permissions = (
+            ('Admin', _('Es el Admin')),
+            ('Usuario', _('Eres un simple usuario')),
+            ('Agregar', _('Puedes agregar productos')),
+            ('Eliminar', _('Puedes eliminar productos')),
+            ('Actualizar', _('Puedes actualizar productos')),
+            ('Buscar', _('Puedes buscar productos')),
+        )
 
