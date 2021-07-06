0742e51c4267458175e72fe72cf30a736169cc57
joshua ramirez <josh.ramirez@duocuc.cl>
2021-07-05 21:50:44 -0400

    Se actualiza el urls

--- pasteleria/urls.py
+++ pasteleria/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("finalizar_pedido_uno", views.finalizar_pedido_uno, name="finalizar_pedido_uno"),
     path("finalizar_pedido_dos", views.finalizar_pedido_dos, name="finalizar_pedido_dos"),
     path("finalizar_pedido_tres", views.finalizar_pedido_tres, name="finalizar_pedido_tres"),
+    
 ]
